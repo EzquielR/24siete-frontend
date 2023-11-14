1edf077467821e4f312cfeb6ff95985e7efff0b1
Luciano Carducci <carduccilucianof@gmail.com>
2023-11-14 20:13:08 -0300

    FrontConfig

---
+++ src/components/landingPage/index.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+	return (
+        <div className="flex justify-center items-center h-screen">
+         <h1>LANDINGPAGE</h1>
+         </div>
+    )
+};
+
+export default LandingPage;
---
+++ src/components/logIn/index.tsx
@@ -0,0 +1,9 @@
+const LogIn = () => {
+	return (
+        <div className="flex justify-center items-center h-screen">
+         <h1>LOGIN</h1>
+         </div>
+    )
+};
+
+export default LogIn;
